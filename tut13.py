72204e22c2bd3076f6def2685779fff66ac1dd2e
praveen-lokhande <praveenlokhande741@gmail.com>
2021-06-19 20:34:32 +0530

    if else statement program

---
+++ tut13.py
@@ -0,0 +1,18 @@
+list=[1,2,3,4]
+print(5 not in list)
+var1=int(input())
+var2=7
+if var1>var2:
+    print("greater")
+else:
+    print("lesser")
+    
+#quize
+print("enter the age:")
+age=int(input())
+if age>18:
+    print("yes you can drive")
+elif age==18:
+    print("we can see about")
+else:
+    print("no you cannot drive")
