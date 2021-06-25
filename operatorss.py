7c6a597eb2599dec30ae01740cbd42b9c1bc546b
praveen-lokhande <praveenlokhande741@gmail.com>
2021-06-25 20:31:06 +0530

    operators

---
+++ operatorss.py
@@ -0,0 +1,58 @@
+#Operator in python
+#Arithmetic operators
+#Assignment operators
+#Comparision operators
+#Logical operators
+#Identity operators
+#Membership operators
+#Bitwise operators
+
+#Arithmetic operators
+print("6+6 is",6+6)
+print("6-6 is",6-6)
+print("6*6 is",6*6)
+print("6/6 is",6/6)
+print("6//6 is",6//6)
+print("6**2 is",6**2)
+print("6%6 is",6%6)
+
+#Assignment operators'=','+=','-=','*=','//=','**=','%='
+a=10
+print(a)
+a+=2
+print(a)
+a-=2
+print(a)
+a*=2
+print(a)
+a//=2
+print(a)
+a%=2
+print(a)
+a**=2
+print(a)
+
+#Comparision operators '==','>=','<=','!=',
+print(a==2)
+print(a>=5)
+print(a<=4)
+print(a!=4)
+
+
+#Logical operators'and','or'
+a=True
+b=False
+print(a and b)
+print(a or b)
+
+#Identity operators'is not'
+print(a is not b)
+
+#Membership operators 'in','not in'
+list=[2,3,4,6,8,3,5,]
+print(2 in list)
+print(10 not in list)
+
+#Bitwise operators'&','|'
+print(0 & 8)
+print(9 | 1)
