3d1b831da66875e3aff295b87c2f3bb3e2d2e93f
praveen-lokhande <praveenlokhande741@gmail.com>
2021-06-28 19:52:00 +0530

    try except pro

---
+++ tut24.py
@@ -0,0 +1,11 @@
+print("Enter num1")
+num1=input()
+print("Enter num2")
+num2=input()
+try:
+    print("The sum of  numbers",
+          int(num1)+int(num2))
+except Exception as e:
+    print(e)
+
+print("this important line")
