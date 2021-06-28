c41ffa48964578526917ccba944698d249d86f56
praveen-lokhande <praveenlokhande741@gmail.com>
2021-06-28 19:52:19 +0530

    file basics

---
+++ tut25.py
@@ -0,0 +1,9 @@
+'''File IO Basics
+"r" - open file for reading
+"W" - open file for writing
+"x" -creates file if not exists
+"a" - add more content to a file
+"t" - text mode
+"b" - binary mode
+"+" - read and write
+'''
