f60896010bd60cb5e57140eb553ae8a1e65c6ba1
praveen-lokhande <praveenlokhande741@gmail.com>
2021-07-19 20:06:09 +0530

    fstring

---
+++ fstring.py
@@ -0,0 +1,16 @@
+#F string
+# import math
+# m1="praveen"
+# m2="pavan"
+# m3="aditya"
+# m4="prateek"
+# #a="this is %s%s%s%s"%(m1,m2,m3,m4)
+# # a="this is {}\t {}\t {}"
+# # b=a.format(m1,m2,m3)
+# c=f"this is {m1} {m2} {m3} {m4} {math.sin(90)} {20+20}"
+# print(c)
+
+a=int(input("enter a value\n"))
+b=int(input("enter b value\n"))
+c=f"the addition of a and b {a+b}"
+print(c)
