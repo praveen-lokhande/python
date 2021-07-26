5a8c1c3ae6e5f9d35146650e7503362b533eb6b8
praveen-lokhande <praveenlokhande741@gmail.com>
2021-07-26 20:17:57 +0530

    time module

---
+++ time123.py
@@ -0,0 +1,16 @@
+import time
+initial=time.time()
+k=0
+while(k<10):
+    print("this is praveen")
+    time.sleep(2)
+    k+=1
+# print(f"while loop ran in {time.time()}-{initial} seconds")
+#
+# initial2=time.time()
+# for i in range(10):
+#     print("this is praveen")
+# print(f"for loop ran in {time.time()}-{initial2}
+
+print(time.asctime(time.localtime()))
+time.
